a2b7b1ef9f82ceaaee41cf011128b2029ac04004
Prodhan5604294 <5604294@sps-student.sk.ca>
2018-09-11 13:58:58 -0600

    update

--- movingRect/sketch.js
+++ movingRect/sketch.js
@@ -4,28 +4,12 @@
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
-
-let x;
-let dx;
-
-
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  x = width/2;
-  dx = 5;
+    createCanvas(400, 400);
+  	background(255);
 }
 
 function draw() {
-  background(150, 150, 150);
-  fill(0, 255, 0);
-  ellipse(mouseX, mouseY, 100, 100);
-
-  //move rectangle
-  x += dx;
-
-  //check if you hit the wall
-  if (x > width-100 || x < 100){
-    dx = dx* -1
-  }
-
+    fill(0);
+    line(pmouseX, pmouseY, mouseX, mouseY);
 }
