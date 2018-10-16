47edea043e6e3df9f7758e24e5ae59ccf8dabfb0
ahmedtalha20 <ahmedtalha20@gmail.com>
2018-10-15 18:28:47 -0600

    p5 folder

---
+++ class-works/interactiveScene/sketch.js
@@ -0,0 +1,22 @@
+// Project Title
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(255);
+}
+
+function draw() {
+  if (mouseIsPressed && keyIsPressed) {
+    if (key === "r"){
+      rect(mouseX, mouseY, 10, 10);
+    }
+    if (key === "e") {
+      ellipse(mouseX, mouseY, 10, 10);
+    }
+  }
+}
