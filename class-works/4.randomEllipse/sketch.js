dd03fa58fd9940de42135d4925865fd868d7b55f
ahmedtalha20 <ahmedtalha20@gmail.com>
2018-10-17 23:40:25 -0600

    random ellipse added

--- class-works/4.randomEllipse/sketch.js
+++ class-works/4.randomEllipse/sketch.js
@@ -7,8 +7,17 @@
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  background(0);
+  noStroke();
 }
 
 function draw() {
-
+  if(mouseX < width/2) {
+    fill(random(255), random(255), random(255));
+    rect(random(width), random(height), random(50), random(50));
+  }else {
+    fill(random(255), random(255), random(255));
+    let circleSize = random(20, 70);
+    ellipse(random(width), random(height), circleSize, circleSize);
+  }
 }
