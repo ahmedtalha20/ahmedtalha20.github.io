f22e0ddaa217049ddb608caa1435bf5e23eda16b
ahmedtalha20 <ahmedtalha20@gmail.com>
2018-10-18 01:15:00 -0600

    image manipulation example

--- class-works/15.imageManipulation/sketch.js
+++ class-works/15.imageManipulation/sketch.js
@@ -1,33 +1,68 @@
 // Image Manipulation Demo
 // Dan Schellenberg
 // Oct 9, 2018
-
 let fish;
 let grayFish;
 
 function preload() {
-  fish = loadImage("assets/fish.jpg");
+  fish = loadImage("assets/fish.jpeg");
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   image(fish, 0, 0);
   grayFish = makeGrayscale(fish);
-  // image(grayFish, 0, 0);
 }
 
 function keyTyped() {
-  if (keyCode === "g") {
-    image(grayFish, 0, 0);
-  }
-  else if(keyCode === "e"){
+  if (key === " ") {
     image(fish, 0, 0);
   }
+  if (key === "g") {
+    image(grayFish, 0, 0);
+  }
+
 }
 
 function draw() {
-
+  image(makeSpotlight(fish), 0, 0);
 }
+
+function makeSpotlight(sourceImage) {
+  let img = createImage(sourceImage.width, sourceImage.height);
+
+  img.loadPixels();
+  sourceImage.loadPixels();
+
+  for (let x = 0; x < sourceImage.width; x++) {
+    for (let y = 0; y < sourceImage.height; y++) {
+      let p = sourceImage.get(x, y);
+
+      let r, g, b;
+
+      if (dist(x, y, mouseX, mouseY) < 50) {
+        //just display pixels
+        r = red(p);
+        g = green(p);
+        b = blue(p);
+      }
+      else {
+        //just show black
+        r = 0;
+        g = 0;
+        b = 0;
+      }
+
+      let newPixel = color(r, g, b);
+
+      img.set(x, y, newPixel);
+    }
+  }
+
+  img.updatePixels();
+  return img;
+}
+
 
 function makeGrayscale(sourceImage) {
   let img = createImage(sourceImage.width, sourceImage.height);
