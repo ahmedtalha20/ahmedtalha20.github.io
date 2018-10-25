bd72cc8e9b3d1e7dd7998736e3fb0aed5725ff0b
Prodhan5604294 <5604294@sps-student.sk.ca>
2018-10-25 14:16:43 -0600

    life of game/grid

--- class-works/18.grid/sketch.js
+++ class-works/18.grid/sketch.js
@@ -7,13 +7,18 @@
 
 let color;
 
-let rows = 5;
-let cols = 5;
+let rows = 10;
+let cols = 10;
 let grid;
 let cellSize
 
 function setup() {
-  createCanvas(600, 600);
+  if (windowWidth > windowHeight) {
+    createCanvas(windowHeight, windowHeight);
+  }
+  else{
+    createCanvas(windowWidth, windowWidth);
+  }
   cellSize = width/cols;
   grid = createRandom2dArray(cols, rows);
 }
@@ -21,6 +26,65 @@ function setup() {
 function draw() {
   background(220);
   displayGrid();
+}
+
+function keyTyped() {
+  if (key === "r") {
+    grid = createRandom2dArray(cols, rows);
+  }
+  else if (key === " ") {
+    update();
+  }
+  else if (key === "c"){
+    resetGrid();
+  }
+}
+
+function update() {
+  // need a second 2d array, so you don't mess up the first one.
+  let nextTurn = [];
+  for (let i = 0; i < rows; i++) {
+    nextTurn[i] = [];
+  }
+
+  //loop through the grid
+  for (let y = 0; y < rows; y++) {
+    for (let x = 0; x < cols; x++) {
+
+      let neighbors = 0;
+
+      for (let i = -1; i <= 1; i++) {
+        for (let j = -1; j <= 1; j++) {
+          if (x+i >= 0 && x+i < cols - 1 && y+j >=0 && y+j < rows) {
+            neighbors += grid[y + j][x + i];
+          }
+        }
+      }
+
+      neighbors -= grid[y][x];
+
+      // applying rules of the game
+      if (grid[y][x] === 1) { // alive
+        if (neighbors === 2 || neighbors === 3) {
+          nextTurn[y][x] = 1;
+        }
+        else {
+          nextTurn[y][x] = 0;
+        }
+      }
+
+      if(grid[y][x] === 0) { //dead
+        if (neighbors === 3) {
+          nextTurn[y][x] = 1;
+        }
+        else {
+          nextTurn[y][x] = 0;
+        }
+      }
+
+    }
+  }
+  grid = nextTurn;
 }
 
 function mousePressed() {
@@ -39,12 +103,20 @@ function displayGrid() {
   for (let y = 0; y < rows; y++) {
     for (let x = 0; x < cols; x++) {
       if (grid[y][x] === 0) {
-        fill(0);
-      }
-      else{
         fill(255);
       }
+      else{
+        fill(0);
+      }
       rect(x*cellSize, y*cellSize, cellSize, cellSize);
+    }
+  }
+}
+
+function resetGrid() {
+  for (let x = 0; x < cols; x++) {
+    for (let y = 0; y < rows; y++){
+      grid[y][x] = 0;
     }
   }
 }
